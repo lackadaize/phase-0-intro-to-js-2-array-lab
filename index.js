7566c63262166da164d58be83b599416192a591e
Jonathan Kulak <lackadaize@msn.com>
2023-11-13 14:11:19 -0500

    This is the git commit for the Arrays Lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+  }
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+  }
+  
+function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+
+function appendCat(name) {
+    let newCatsAppend = [...cats, "Broom"];
+    return newCatsAppend;
+  }
+
+function prependCat(name) {
+    let newCatsPrepend = ["Arnold", ...cats,];
+    return newCatsPrepend;
+  }
+
+function removeLastCat() {
+    let newRemoveLastCat = cats.slice(0, -1);
+    return newRemoveLastCat;
+}
+
+function removeFirstCat() {
+    let newRemoveFirstCat = cats.slice(1);
+    return newRemoveFirstCat;
+}
